b95bb26ca04107ab5f19f448efefaa4275e28885
Maxito06 <maxicopi5@gmail.com>
2023-02-01 19:14:13 -0500

    login + permisos

--- prueba/settings.py
+++ prueba/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path 
 import os
 
+from libreria.settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL ='/'
+LOGOUT_REDIRECT_URL ='/accounts/login/'
+
